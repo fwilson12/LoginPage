c1ebf73541024a0c251c367ac8d40f2bd69e136e
s10043679 <fletcherbwilson1@gmail.com>
2023-09-28 10:30:43 -0500

    first commit 9.28  question, error, correct screens

---
+++ main.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class LoginPageApp(App):
+    pass
+
+class ErrorScreen(Screen):
+    pass
+
+class CorrectScreen(Screen):
+    pass
+
+
+class LoginManager(ScreenManager):
+    pass
+
+class Question1Screen(Screen):
+    pass
+
+
+
+
+LoginPageApp().run()
