1b3679ac741c2cc7f26f592b62c4baaca68983a0
s10043679 <fletcherbwilson1@gmail.com>
2023-09-29 10:30:20 -0500

    second commit 9.28 2 quiz questions

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-
-class LoginPageApp(App):
-    pass
+Builder.load_file("LoginPage.kv")
+class QuizPageApp(App):
+    def build(self):
+        return QuizManager()
 
 class ErrorScreen(Screen):
-    pass
+    def next_question(self):
+        self.manager.current = 'question2'
 
 class CorrectScreen(Screen):
-    pass
+    def next_question(self):
+        self.manager.current = 'question2'
 
-
-class LoginManager(ScreenManager):
+class QuizManager(ScreenManager):
     pass
 
 class Question1Screen(Screen):
-    pass
+    def answer_question(self, bool):
+        if bool:
+            self.manager.current = 'correct'
+        else:
+            self.manager.current = 'wrong'
+class Question2Screen(Screen):
+    def answer_question(self, bool):
+        if bool:
+            self.manager.current = 'correct'
+        else:
+            self.manager.current = 'wrong'
 
 
 
 
-LoginPageApp().run()
+QuizPageApp().run()
