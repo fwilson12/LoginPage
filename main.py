356021f94aa977299710cc548914beeefe630ffa
s10043679 <fletcherbwilson1@gmail.com>
2023-10-03 10:33:10 -0500

    revolutionary commit: login page is working. 10/3

--- main.py
+++ main.py
@@ -3,35 +3,67 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 Builder.load_file("LoginPage.kv")
-class QuizPageApp(App):
+Builder.load_file("LoginPage2.kv")
+
+# class QuizPageApp(App):
+#     def build(self):
+#         return QuizManager()
+#
+# class ErrorScreen(Screen):
+#     def next_question(self):
+#         self.manager.current = 'question2'
+#
+# class CorrectScreen(Screen):
+#     def next_question(self):
+#         self.manager.current = 'question2'
+#
+# class QuizManager(ScreenManager):
+#     pass
+#
+# class Question1Screen(Screen):
+#     def answer_question(self, bool):
+#         if bool:
+#             self.manager.current = 'correct'
+#         else:
+#             self.manager.current = 'wrong'
+# class Question2Screen(Screen):
+#     def answer_question(self, text):
+#         if text.lower() == 'curve':
+#             self.manager.current = 'correct'
+#         else:
+#             self.ids.invalid_guess.text = 'did you get a lobotomy?'
+#             self.ids.invalid_guess.color = 'green'
+#
+#
+#
+#
+#
+# QuizPageApp().run()
+
+
+class LoginPage2App(App):
     def build(self):
-        return QuizManager()
+        return LoginManager()
 
-class ErrorScreen(Screen):
-    def next_question(self):
-        self.manager.current = 'question2'
 
-class CorrectScreen(Screen):
-    def next_question(self):
-        self.manager.current = 'question2'
-
-class QuizManager(ScreenManager):
+class LoginManager(ScreenManager):
     pass
 
-class Question1Screen(Screen):
-    def answer_question(self, bool):
-        if bool:
-            self.manager.current = 'correct'
+class LoginScreen(Screen):
+    def login(self, user, password):
+        if user in users.keys() and password in users.values():
+            self.manager.current = 'logout'
         else:
-            self.manager.current = 'wrong'
-class Question2Screen(Screen):
-    def answer_question(self, bool):
-        if bool:
-            self.manager.current = 'correct'
-        else:
-            self.manager.current = 'wrong'
+            self.ids.invalid_credentials.text = 'register a new account'
+            self.ids.invalid_credentials.color = 'blue'
 
 
+class RegisterScreen(Screen):
+    pass
 
+class LogoutScreen(Screen):
+    def logout(self):
+        self.manager.current = 'login'
 
-QuizPageApp().run()
+users = {'krazyman50': 'geometryDasher87!', 'belugawhale': 'OceanCreature87%'}
+LoginPage2App().run()
